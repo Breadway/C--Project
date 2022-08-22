8f8e804f63f22e61446177cc2363e2667600504c
Breadway <108389940+Breadway@users.noreply.github.com>
2022-08-22 13:57:47 +0800

    Update Server.lua

--- Secure-Remote/Server.lua
+++ Secure-Remote/Server.lua
@@ -1,1 +1,17 @@
-local 
+local remote = game.ReplicatedStorage.Packages.SecureRemote
+local y = math.random(1/9) .. math.random(1/9) .. math.random(1/9) .. math.random(1/9) .. math.random(1/9)
+local key = math.random(1/99999)
+
+repeat
+  y = math.random(1/9) .. math.random(1/9) .. math.random(1/9) .. math.random(1/9) .. math.random(1/9)
+until y == key
+
+remote.OnServerEvent:Connect(function(x, args)
+    local z = x*y
+    if z == game.RepllicatedStorage.Value.Value * y then
+        --Event
+      runcode(function()
+          args
+      end)
+    end
+end)
