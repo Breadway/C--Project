491c15447e143a0f9b5836f8f5a92d1c95ac357a
Breadway <108389940+Breadway@users.noreply.github.com>
2022-08-22 08:18:03 +0800

    Create Game2.lua

---
+++ Game2.lua
@@ -0,0 +1,21 @@
+script.Parent:WaitForChild("player").Value = game.Players.LocalPlayer.Name
+local var = require(script.Parent.Script)
+local Int = var.Intvars()
+local Bool = var.Boolvars()
+local Int = var.Intvars()
+local Int = var.Intvars()
+local Int = var.Intvars()
+local Int = var.Intvars()
+local Int = var.Intvars()
+local Int = var.Intvars()
+local Int = var.Intvars()
+local Int = var.Intvars()
+local Int = var.Intvars()
+
+var.new({
+	["Type"] = "Bool",
+	["Name"] = "e",
+	["Value"] = true
+})
+
+print(vars.Bool.e.Value)
