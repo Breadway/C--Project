779699f71fd1c26f44a16797415e324981425136
Breadway <108389940+Breadway@users.noreply.github.com>
2022-08-22 14:39:56 +0800

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -1,8 +1,8 @@
 local Sew = loadstring(game.ReplicatedStorage.Packages.Sew) or game.ReplicatedStorage.Packages.Sew
 local Vars = Sew.Intvars() or Sew.Boolvars() or Sew.Brickvars() or Sew.CFramevars() or Sew.Colorvars() or Sew.Objectvars() or Sew.Numbervars() or Sew.Rayvars() or Sew.Vector3vars() or Sew.Stringvars()
 local getVars = Sew.GetVars()
-local getAllServices = Sew.GetAllServices()
-local Sew.Controllers = Sew.GetAllControllers
+local Sew.Services = Sew.GetAllServices()
+local Sew.Controllers = Sew.GetAllControllers()
 
 Sew.CreateVar({
 	["Type"] = "Value",
