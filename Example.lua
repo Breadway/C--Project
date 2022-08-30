2cbe5acd879b5a03c2346729cb062d4c6978c12a
Breadway <108389940+Breadway@users.noreply.github.com>
2022-08-30 09:49:42 +0800

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -1,4 +1,6 @@
-local Sew = loadstring(game.ReplicatedStorage.Packages.Sew) or game.ReplicatedStorage.Packages.Sew
+local Sew = require(game.ReplicatedStorage.Packages.Sew)
+
+print(Sew)
 
 Sew.CreateVar({
 	["Type"] = "Int",
@@ -6,20 +8,28 @@ Sew.CreateVar({
 	["Value"] = 0
 })
 
-Sew.CreateService({["Name"] = "SwordService"})
-local SwordService = Sew.Services["SwordService"]
+Sew.CreateService("SwordService")
+local SwordService = Sew.Services.SwordService
 
 Sew.CreateController({
 	["Name"] = "SwordController",
-	["ParentService"] = Sew.Services["SwordService"],
+	["ParentService"] = SwordService,
 	["Event"] = function(args)
 		print(args)
 		Value.Value += args
 	end)
 })
 
+for i, v in pairs(Sew.Services:GetDecendants()) do
+	print(v.Name, v.Parent)
+end
+
+for i, v in pairs(Sew.Vars:GetChildren()) do
+	print(v.Name, v.Parent)
+end
+
 local args = 5
 
-Sew.Controllers["SwordController"]:Fire(args)
+SwordService.SwordController:Fire(args)
 
 print(Sew.Vars["Value"])
